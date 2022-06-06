936e9386b7b189eb0c67f30564db4166fd432d03
fpb1984 <fpb1984@gmail.com>
2022-06-06 20:30:47 -0300

    Update forti-vpn.sh

--- forti-vpn.sh
+++ forti-vpn.sh
@@ -2,10 +2,10 @@
 
 CONNECT_PID=""
 RUNNING="" 
-VPN_HOST="vpnkcc.kcl.cl:443"
-VPN_USER="r.picon_redhat"
-VPN_PASS="Lurgd#4982j"
-FORTICLIENT_PATH="opt/forticlient-sslvpn/64bit/forticlientsslvpn_cli"
+set VPN_HOST="vpnkcc.kcl.cl:443"
+set VPN_USER="r.picon_redhat"
+set VPN_PASS="Lurgd#4982j"
+set FORTICLIENT_PATH="opt/forticlient-sslvpn/64bit/forticlientsslvpn_cli"
  
 # Checks whether vpn is connected
 function checkConnect {
@@ -17,7 +17,7 @@ function checkConnect {
 function startConnect {
  
     # start vpn connection and grab its pid (expect script returns spawned vpn conn pid)
-    CONNECT_PID=connect
+    CONNECT_PID="connect"
     eval $CONNECT_PID
 }
  
