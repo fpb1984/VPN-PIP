c2f49d1ab01f88c6108e20e22655f150a02b28c1
fpb1984 <fpb1984@gmail.com>
2022-06-07 11:10:41 -0300

    Update forti-vpn.sh

--- forti-vpn.sh
+++ forti-vpn.sh
@@ -15,18 +15,13 @@ function checkConnect {
  
 # Initiates connection
 function startConnect {
- 
-    # start vpn connection and grab its pid (expect script returns spawned vpn conn pid)
     CONNECT_PID="connect"
     eval $CONNECT_PID
 }
  
 # Creates an expect script to complete automated vpn connection
 function connect {
- 
-    # write expect script to tmp location
-    cat <<-EOF > /tmp/expect
-        #!/usr/bin/expect -f
+     cat <<-EOF > /tmp/expect
         match_max 1000000
         set timeout -1
         spawn $FORTICLIENT_PATH --server $VPN_HOST --vpnuser $VPN_USER --keepalive
@@ -34,24 +29,14 @@ function connect {
         expect "Password for VPN:"
         send -- "$VPN_PASS"
         send -- "\r"
- 
         expect "Would you like to connect to this server? (Y/N)"
         send -- "Y"
         send -- "\r"
- 
         expect "Clean up..."
         close
 	EOF
-     
-    #IMPORTANT!: the "EOF" just above must be preceded by a TAB character (not spaces)
- 
-    # lock down and execute expect script
     chmod 500 /tmp/expect
     /usr/bin/expect -f /tmp/expect
- 
-    # when expect script is finished (closes) clean up
     rm -f /tmp/expect
 }
- 
 startConnect
- 
